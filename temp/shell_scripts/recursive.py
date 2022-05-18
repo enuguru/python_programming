e6142e91786a833a78ba3a08edbfe90fb9d2f660
your name <guruforsure@gmail.com>
2022-05-18 17:34:01 +0530

    new files

---
+++ temp/shell_scripts/recursive.py
@@ -0,0 +1,8 @@
+
+
+def fibonacci_of(n):
+    if n in {0, 1}:  # Base case
+        return n
+    return fibonacci_of(n - 1) + fibonacci_of(n - 2)  # Recursive case
+
+print([fibonacci_of(n) for n in range(15)])
