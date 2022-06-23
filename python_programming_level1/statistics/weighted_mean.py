51d28b6ed563e5ab25ba265ecc7af4ae11db1149
your name <guruforsure@gmail.com>
2022-06-23 10:36:39 +0530

    all files

--- python_programming_level1/statistics/weighted_mean.py
+++
@@ -1,9 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-
-val = int(input())
-arraylist = list(map(int,input().split()))
-weightlist = list(map(int,input().split()))
-mysum = 0
-for i in range(val): 
-    mysum = mysum + (arraylist[i] * weightlist[i])
-print(round(mysum/sum(weightlist),1))
