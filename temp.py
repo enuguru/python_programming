f88bd03756c6f3db32cf3854afb1bb4171f4feb9
enuguru <guruforsure@gmail.com>
2022-12-22 21:53:27 +0530

    all files

--- temp.py
+++
@@ -1,9 +0,0 @@
-
-
-import functools
-
-def ss_decode_col_id(col):
-    return functools.reduce(lambda result,c: result*26 + ord(c) - ord('A'),col,0)
-
-excelcolid = input("Give the Column id ")
-print(ss_decode_col_id(excelcolid))
